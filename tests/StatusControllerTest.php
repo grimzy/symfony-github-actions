38d66b2b7047f94ec74c0d1eb4c72f9bac06f1d2
Joseph Estefane <estefanejoe@gmail.com>
2020-04-25 19:42:04 -0400

    Added StatusController and StatusControllerTest

---
+++ tests/StatusControllerTest.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace App\Tests;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class StatusControllerTest extends WebTestCase
+{
+    public function testStatusOk()
+    {
+        $client = static::createClient();
+        $client->request('GET', '/status');
+
+        $this->assertResponseIsSuccessful();
+        $response = $client->getResponse();
+        $this->assertEquals('{"status":"ok"}', $response->getContent());
+    }
+}
